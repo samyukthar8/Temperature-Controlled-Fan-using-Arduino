3c56b8bf3fbbbe9e8770b08ce8141c208287d220
samyukthar8 <63106738+samyukthar8@users.noreply.github.com>
2022-08-15 17:37:42 +0530

    Update tempard.ino

--- tempard.ino
+++ tempard.ino
@@ -3,6 +3,7 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 float temp;
 int tempPin = A0;
 #define fan 9
+
 void setup () ;
 pinMode (fan, OUTPUT);
 lcd.begin(16,3);
@@ -14,6 +15,7 @@ lcd.print("AUTO TEMPERATURE");
 delay(2000);
 lcd.clear();
 }
+
 void loop()
 {
 lcd.setCursor(3, 0);
@@ -24,8 +26,6 @@ delay(3000);
 lcd.clear();
 lcd.setCursor(0, 2);
 temp = analogRead(tempPin);
-
-
 temp = temp * 0.2;
 temp = temp - 5;
 lcd.setCursor(0, 0);
@@ -34,6 +34,7 @@ lcd.setCursor(5, 1);
 lcd.print(temp); //Printing Temp
 delay(3000);
 lcd.clear();
+  
 if(temp <=41.00)
 {
 analogWrite(9, 0);
@@ -41,6 +42,7 @@ lcd.print("Fan Off");
 delay(2000);
 lcd.clear();
 }
+  
 else if(temp <=41.20)
 {
 analogWrite(fan, 60);
@@ -53,131 +55,141 @@ else if(temp <=41.40)
 analogWrite(fan, 60);
 lcd.print("Fan Speed: 20% ");
 delay(2000);
-
-
 lcd.clear(); }
+  
 else if(temp <=41.60) {
 analogWrite(fan, 60);
 lcd.print("Fan Speed: 20% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp <=41.80) {
 analogWrite(fan, 60);
 lcd.print("Fan Speed: 20% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=42.00) {
 analogWrite(fan, 88);
 lcd.print("Fan Speed: 40% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=42.20) {
 analogWrite(fan, 88);
 lcd.print("Fan Speed: 40% ");
-
-
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=42.40) {
 analogWrite(fan, 88);
 lcd.print("Fan Speed: 40% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=42.60) {
 analogWrite(fan, 88);
 lcd.print("Fan Speed: 40% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=42.80) {
 analogWrite(fan, 88);
 lcd.print("Fan Speed: 40% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=43.00) {
 analogWrite(fan, 116);
-
-
 lcd.print("Fan Speed: 60% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=43.20) {
 analogWrite(fan, 116);
 lcd.print("Fan Speed: 60% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=43.40) {
 analogWrite(fan, 116);
 lcd.print("Fan Speed: 60% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=43.60) {
 analogWrite(fan, 116);
 lcd.print("Fan Speed: 60% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=43.80) {
-
-
-
 analogWrite(fan, 116);
 lcd.print("Fan Speed: 60% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=44.00) {
 analogWrite(fan, 144);
 lcd.print("Fan Speed: 80% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=44.20) {
 analogWrite(fan, 144);
 lcd.print("Fan Speed: 80% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=44.40) {
 analogWrite(fan, 144);
 lcd.print("Fan Speed: 80% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=44.60)
-
-
 {
 analogWrite(fan, 144);
 lcd.print("Fan Speed: 80% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=44.80) {
 analogWrite(fan, 144);
 lcd.print("Fan Speed: 80% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=45.00) {
 analogWrite(fan, 172);
 lcd.print("Fan Speed: 90% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=45.20) {
 analogWrite(fan, 172);
 lcd.print("Fan Speed: 90% ");
 delay(2000);
 lcd.clear(); }
-
-
+  
 else if(temp<=45.40) {
 analogWrite(fan, 172);
 lcd.print("Fan Speed: 90% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=45.60) {
 analogWrite(fan, 172);
 lcd.print("Fan Speed: 90% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp<=45.80) {
 analogWrite(fan, 172);
 lcd.print("Fan Speed: 90% ");
 delay(2000);
 lcd.clear(); }
+  
 else if(temp>=46.00) {
 analogWrite(fan, 200);
 lcd.print("Fan Speed: 100% ");
